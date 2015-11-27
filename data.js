3b06e395c356c77835981a21374451bb663a0aec
Mike <mikemiles86@gmail.com>
2015-11-27 16:26:44 -0500

    Update data.js

--- data.js
+++ data.js
@@ -1,78 +1,77 @@
 var trip = {
   "dates" : {
-    "saturday" : { "start" : 9, "end" : 22},
-    "sunday" : {"start": 9, "end" : 22},
-    "monday" : {"start": 9, "end" : 22},
-    "tuesday" : {"start": 9, "end" : 22},
-    "wednesday": {"start": 9, "end" : 22},
-    "thursday": {"start": 9, "end" : 22},
-    "friday" : {"start": 9, "end" : 22}
+    "saturday" : { "start" : 9, "end" : 22.5},
+    "sunday" : {"start": 9, "end" : 22.5},
+    "monday" : {"start": 9, "end" : 22.5},
+    "tuesday" : {"start": 9, "end" : 22.5},
+    "wednesday": {"start": 9, "end" : 22.5},
+    "thursday": {"start": 9, "end" : 22.5},
+    "friday" : {"start": 9, "end" : 22.5}
   },
   "max_acts_per_day" : 3,
-  "buffer" : 1,
+  "buffer" : 1.5,
+  "adjust_by" : 1,
+  "max_options" : 20,
 }
 
 var activities = {
+  
   "travel_to" : {
     "name" : "Travel",
-    "length" : 4,
+    "length" : 3.5,
     "dates" : {
-      "saturday" : [{"start" : 10, "end" : 14}]
+      "saturday" : [{"start" : 10.5, "end" : 14}]
     }
   },
   "travel_from" : {
     "name" : "Travel",
-    "length" : 10,
+    "length" : 9,
     "dates" : {
-      "saturday" : [{"start" : 12, "end" : 22}]
+      "friday" : [{"start" : 12, "end" : 20}]
     }
-  },
+  },/*
   "winter_market" : {
     "name" : "Winter Market",
     "length" : 2,
     "dates" : {
-      "saturday" : [{ "start" : 10, "end" : 22}],
-      "sunday" : [{"start": 10, "end" : 22}],
-      "monday" : [{"start": 10, "end" : 22}],
-      "tuesday" : [{"start": 10, "end" : 22}],
-      "wednesday": [{"start": 10, "end" : 22}],
-      "thursday": [{"start": 10, "end" : 22}],
-      "friday" : [{"start": 10, "end" : 22}]
+      "saturday" : [{ "start" : 10, "end" : 21}],
+      "sunday" : [{"start": 10, "end" : 21}],
+      "monday" : [{"start": 10, "end" : 21}],
+      "tuesday" : [{"start": 10, "end" : 21}],
+      "wednesday": [{"start": 10, "end" : 21}],
+      "thursday": [{"start": 10, "end" : 21}]
     }
-  },
+  },*/
   "kerst_fair" : {
     "name" : "Kerst Fair Market",
     "length" : 2,
     "dates" : {
       "sunday" : [{ "start": 12, "end": 18}]
     }
-  },
+  },/*
   "coconuts" : {
     "name" : "Coffee & Coconuts",
     "length" : 1.5,
     "dates" : {
-      "saturday" : [{ "start" : 9, "end" : 22}],
-      "sunday" : [{"start": 9, "end" : 22}],
-      "monday" : [{"start": 9, "end" : 22}],
-      "tuesday" : [{"start": 9, "end" : 22}],
-      "wednesday": [{"start": 9, "end" : 22}],
-      "thursday": [{"start": 9, "end" : 22}],
-      "friday" : [{"start": 9, "end" : 22}]
+      "sunday" : [{"start": 10, "end" : 21}],
+      "monday" : [{"start": 10, "end" : 21}],
+      "tuesday" : [{"start": 10, "end" : 21}],
+      "wednesday": [{"start": 10, "end" : 21}],
+      "thursday": [{"start": 10, "end" : 21}]
     }
   },
   "chocolate" : {
     "name" : "Metropolitan Chocolates",
     "length" : 1,
     "dates" : {
-      "saturday" : [{ "start" : 9, "end" : 22}],
-      "sunday" : [{"start": 9, "end" : 22}],
-      "monday" : [{"start": 9, "end" : 22}],
-      "tuesday" : [{"start": 9, "end" : 22}],
-      "wednesday": [{"start": 9, "end" : 22}],
-      "thursday": [{"start": 9, "end" : 22}],
-      "friday" : [{"start": 9, "end" : 22}]
+      "saturday" : [{ "start" : 10, "end" : 21}],
+      "sunday" : [{"start": 10, "end" : 21}],
+      "monday" : [{"start": 10, "end" : 21}],
+      "tuesday" : [{"start": 10, "end" : 21}],
+      "wednesday": [{"start": 10, "end" : 21}],
+      "thursday": [{"start": 10, "end" : 21}]
     }
-  },
+  },*/
   "raypenaeyr": {
     "name" : "Raypenaeyr Cheese Tasting",
     "length" : 1,
@@ -106,12 +105,6 @@ var activities = {
         {"start" : 13.5, "end" : 14.5},
         {"start" : 15, "end" : 16},
         {"start" : 16.5, "end" : 17.5}
-      ],
-      "friday" : [
-        {"start" : 12, "end" : 13},
-        {"start" : 13.5, "end" : 14.5},
-        {"start" : 15, "end" : 16},
-        {"start" : 16.5, "end" : 17.5}
       ]
     }
   },
@@ -126,82 +119,69 @@ var activities = {
     "name" : "Paradox Cafe",
     "length" : 2,
     "dates" : {
-      "saturday" : [{"start" : 10, "end" : 20}],
-      "sunday" : [{"start" : 10, "end" : 20}],
-      "monday" : [{"start" : 10, "end" : 20}],
-      "wednesday" : [{"start" : 10, "end" : 20}]
+      "saturday" : [{"start" : 15, "end" : 20}],
+      "sunday" : [{"start" : 15, "end" : 20}],
+      "monday" : [{"start" : 15, "end" : 20}],
+      "wednesday" : [{"start" : 15, "end" : 20}]
     }
   },
   "illuminade" : {
     "name" : "Illuminade",
     "length" : 3,
     "dates" : {
-      "thursday" : [{"start" : 17, "end" : 22}]
+      "thursday" : [{"start" : 17, "end" : 21}]
     }
-  },
+  },/*
   "cheese_museum" : {
     "name" : "Cheese Museum",
-    "length" : .5,
+    "length" : 0.5,
     "dates" : {
-      "saturday" : [{ "start" : 9, "end" : 20}],
-      "sunday" : [{"start": 9, "end" : 20}],
-      "monday" : [{"start": 9, "end" : 20}],
-      "tuesday" : [{"start": 9, "end" : 20}],
-      "wednesday": [{"start": 9, "end" : 20}],
-      "thursday": [{"start": 9, "end" : 20}],
-      "friday" : [{"start": 9, "end" : 20}]
+      "saturday" : [{ "start" : 10, "end" : 20}],
+      "sunday" : [{"start": 10, "end" : 20}],
+      "monday" : [{"start": 10, "end" : 20}],
+      "tuesday" : [{"start": 10, "end" : 20}],
+      "wednesday": [{"start": 10, "end" : 20}],
+      "thursday": [{"start": 10, "end" : 20}]
     }
-  },
+  },*/
   "anne_frank" : {
     "name" : "Anne Frank House",
     "length" : 1.5,
     "dates" : {
       "tuesday" : [{"start" : 11.5, "end" : 13}]
     }
-  },
+  }/*,
   "Rijks" : {
     "name" : "Rijks Museum",
     "length" : 3,
     "dates" : {
-      "saturday" : [{ "start" : 9, "end" : 17}],
-      "sunday" : [{"start": 9, "end" : 17}],
-      "monday" : [{"start": 9, "end" : 17}],
-      "tuesday" : [{"start": 9, "end" : 17}],
-      "wednesday": [{"start": 9, "end" : 17}],
-      "thursday": [{"start": 9, "end" : 17}],
-      "friday" : [{"start": 9, "end" : 17}]
+      "sunday" : [{"start": 10, "end" : 17}],
+      "monday" : [{"start": 10, "end" : 17}],
+      "tuesday" : [{"start": 10, "end" : 17}],
+      "wednesday": [{"start": 10, "end" : 17}],
+      "thursday": [{"start": 10, "end" : 17}]
     }
   },
   "gogh" : {
     "name" : "Van Gogh Museum",
     "length" : 3,
     "dates" : {
-      "saturday" : [{ "start" : 9, "end" : 17}],
-      "sunday" : [{"start": 9, "end" : 17}],
-      "monday" : [{"start": 9, "end" : 17}],
-      "tuesday" : [{"start": 9, "end" : 17}],
-      "wednesday": [{"start": 9, "end" : 17}],
-      "thursday": [{"start": 9, "end" : 17}],
-      "friday" : [{"start": 9, "end" : 17}]
+      "sunday" : [{"start": 10, "end" : 17}],
+      "monday" : [{"start": 10, "end" : 17}],
+      "tuesday" : [{"start": 10, "end" : 17}],
+      "wednesday": [{"start": 10, "end" : 17}],
+      "thursday": [{"start": 10, "end" : 17}]
     }
   },
   "micro" : {
     "name" : "Micropia",
     "length" : 1.5,
     "dates" : {
-      "saturday" : [{ "start" : 9, "end" : 18}],
-      "sunday" : [{"start": 9, "end" : 18}],
-      "monday" : [{"start": 9, "end" : 18}],
-      "tuesday" : [{"start": 9, "end" : 18}],
-      "wednesday": [{"start": 9, "end" : 18}],
-      "thursday": [{"start": 9, "end" : 18}],
-      "friday" : [{"start": 9, "end" : 18}]
+      "sunday" : [{"start": 10, "end" : 18}],
+      "monday" : [{"start": 10, "end" : 18}],
+      "tuesday" : [{"start": 10, "end" : 18}],
+      "wednesday": [{"start": 10, "end" : 18}],
+      "thursday": [{"start": 10, "end" : 18}]
     }
-  }
+  }*/
 }
-
-
-
-
-
-
